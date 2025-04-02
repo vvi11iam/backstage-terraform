7c3ea75034f57af9d0b8bc256cfc3b2f38bc5070
vvi11iam <116747419+vvi11iam@users.noreply.github.com>
2025-04-02 23:26:24 +0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "remote" {
+    organization = "romthayon2310"
+
+    workspaces {
+      name = "github-actions"
+    }
+  }
+}
+
+# An example resource that does nothing.
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
