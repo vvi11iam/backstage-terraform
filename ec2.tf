9087466f9cc7878fc2a699c9363fc1ec471c3621
vvi11iam <116747419+vvi11iam@users.noreply.github.com>
2025-04-02 15:33:56 +0700

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.10.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+
 data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
